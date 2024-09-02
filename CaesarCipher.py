e9c06bcab4cb41d062d12de489d6b468108e32f6
unknown <ddu@zagmail.gonzaga.edu>
2024-09-02 11:06:09 +0800

    update

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,16 +1,45 @@
-# Author: 
+# Author: Danni Du
 import random
 import string
 
 # create and print the key
 def CreateKey():
     # TODO
-    pass
+    key = random.randint(0,26)
+    print(f"this is the key for Caesar Cipher : {key}")
+    return key
 
 # print the encrypted strings and how you get it
 def encrypt(letters):
     # TODO
-    pass
+    encrypt_text = ""
+    key = CreateKey()
+    Letter = "A"
+    Letternum = 0
+    Encryptletter = "A"
+    Encryptnum = 0
+    letters = letters.upper()
+    for l in letters:
+        if l == " ":
+            Letternum = 26
+            Encryptnum = (26 + key) % 27
+            if Encryptnum == 26:
+                Encryptletter = " "
+            else:
+                Encryptnum += ord(Letter)
+                Encryptletter = chr(Encryptnum)    
+        else:
+            Encryptnum = (ord(l) - ord(Letter) + key) % 27
+            if Encryptnum == 26:
+                Encryptletter = " "
+            else:
+                Encryptnum += ord(Letter)
+                Encryptletter = chr(Encryptnum) 
+        encrypt_text = encrypt_text + Encryptletter
+    print(f"Encrypted letters: {encrypt_text}")
+    return encrypt_text
+        
+
 
 # print the decrypted strings and how you get it
 # check if the key is legal
