d867e18e0663ff64864003236ef4bc7dded65b7a
Kyle Connall <kconnall2@zagmail.gonzaga.edu>
2024-09-03 20:55:49 -0700

    length update for vigciph

--- VigenèreCipher.py
+++ VigenèreCipher.py
@@ -15,18 +15,18 @@ numDict = {"0": "A", "1": "B", "2": "C", "3": "D", "4": "E", "5": "F", "6": "G",
            "10": "K", "11": "L", "12": "M","13": "N", "14": "O", "15": "P", "16": "Q", "17": "R", "18": "S",
             "19": "T", "20": "U", "21": "V", "22": "W", "23": "X", "24": "Y","25": "Z", "26": " "}
 # create and print the key
-def CreateKey(message,keylen = 4):
+def CreateKey(message,keylen):
     letters = string.ascii_uppercase + ' '
     # Create the key to the length of 4
     key1 = ''.join(random.choice(letters) for i in range(keylen))
     # Match the key to the length of the message by using it over and over again.
     # If the key is shorter than the message, it will repeat.
     if len(message) > keylen:
-        key = ''.join(key1[i % keylen] for i in range(len(message)))
+        keymask = ''.join(key1[i % keylen] for i in range(len(message)))
     elif len(message) <= keylen:
-        key = key1[:len(message)]
-    print(key1)
-    return key
+        keymask = key1[:len(message)]
+    print(f"Key for Vignere Cipher: {key1}") #print actual key, return keymask.
+    return keymask
 
 
 # print the encrypted strings and how you get it
@@ -36,24 +36,28 @@ def encrypt(message, length):
     #Pass the message to the dictionary to convert letters to numbers.
     #Add the numbers and mod 27 to get the encrypted message
     #Append the encrypted message to the list
-    key1 = CreateKey(message)
+    key1 = CreateKey(message,length)
     for i in range(len(message)):
         messageToNum = letterDict[message[i]]
         keyToNum = letterDict[key1[i]]
         encryptedToNum = (int(messageToNum) + int(keyToNum)) % 27
         #pass back into the reverse dictionary to get the letters back
         encrypted.append(numDict[str(encryptedToNum)])
+        print(f"{int(messageToNum)} + {int(keyToNum)} % 27 = {encryptedToNum}")
     return ''.join(encrypted)
 
 # print the decrypted strings and how you get it
 # check if the key is legal
 def decrypt(message, key):
     decrypted = []
-
+    keylen  = len(key)
+    # Match the key to the length of the message by using it over and over again.
+    key1 = ''.join(key[i % keylen] for i in range(len(message)))
     for i in range(len(message)):
         messageToNum = letterDict[message[i]]
-        keyToNum = letterDict[key[i]]
+        keyToNum = letterDict[key1[i]]
         decryptedToNum = (int(messageToNum) - int(keyToNum)) % 27
         decrypted.append(numDict[str(decryptedToNum)])
+        print(f"{int(messageToNum)} - {int(keyToNum)} % 27 = {decryptedToNum}")
     print(decrypted)
     return ''.join(decrypted)
